6ac2b4b8fa9b0c2c2dfe9d1f009f3d99bcf27ce8
lining-lo <2092576148@qq.com>
2026-08-08 11:10:16 +0800

    feat: 新增应用结构化配置模块，yaml + dataclass模型解析

---
+++ app/conf/app_config.py
@@ -0,0 +1,104 @@
+"""
+  @Author:lining-lo
+  @Time:2026/8/8
+  @Desc: 解析conf/app_config.yaml文件, 生成指定类型的对象
+"""
+from pathlib import Path
+from omegaconf import OmegaConf
+
+from pydantic.dataclasses import dataclass
+
+
+# ==================== 日志配置模型 ====================
+@dataclass
+class File:
+    enable: bool
+    level: str
+    path: str
+    rotation: str
+    retention: str
+
+
+@dataclass
+class Console:
+    enable: bool
+    level: str
+
+
+@dataclass
+class LoggingConfig:
+    file: File
+    console: Console
+
+
+# ==================== database配置模型 ====================
+
+@dataclass
+class DBConfig:
+    host: str
+    port: int
+    user: str
+    password: str
+    database: str
+
+
+# ==================== Qdrant 配置模型 ====================
+
+@dataclass
+class QdrantConfig:
+    host: str
+    port: int
+    embedding_size: int
+
+
+# ==================== Embedding 配置模型 ====================
+
+@dataclass
+class EmbeddingConfig:
+    host: str
+    port: int
+    model: str
+
+
+# ==================== ES 配置模型 ====================
+
+@dataclass
+class ESConfig:
+    host: str
+    port: int
+    index_name: str
+
+
+# ==================== LLM 配置模型 ====================
+
+@dataclass
+class LLMConfig:
+    model_name: str
+    api_key: str
+
+
+# ==================== 应用总配置模型 ====================
+
+@dataclass
+class AppConfig:
+    logging: LoggingConfig
+    db_meta: DBConfig
+    db_dw: DBConfig
+    qdrant: QdrantConfig
+    embedding: EmbeddingConfig
+    es: ESConfig
+    llm: LLMConfig
+
+
+# 得到yaml配置文件的绝对路径
+_yaml_path = Path(__file__).parents[2] / "conf" / "app_config.yaml"
+
+# 根据路径加载配置文件数据 =》DictConfig对象
+_yaml_data = OmegaConf.load(_yaml_path)
+
+# 将数据转换为AppConfig类型的对象
+app_config: AppConfig = OmegaConf.to_object(OmegaConf.merge(AppConfig, _yaml_data))
+
+if __name__ == "__main__":
+    print(app_config)
+    print(app_config.logging.console.level)
