e794e8b789a612712189b5ada144782bc4f9ccd5
lining-lo <2092576148@qq.com>
2026-08-14 14:37:22 +0800

    feat: 实现"校正sql节点"业务逻辑

--- app/agent/nodes/correct_sql.py
+++ app/agent/nodes/correct_sql.py
@@ -1,18 +1,55 @@
 """
   @Author:lining-lo
   @Time:2026/8/12
-  @Desc:
+  @Desc:校正sql节点，
+        收集错误信息，调用大模型矫正sql
 """
+import yaml
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
 from langgraph.runtime import Runtime
 from app.agent.context import DataAgentContext
+from app.agent.llm import llm
 from app.agent.state import DataAgentState
 from app.core.log import logger
+from app.prompt.prompt_loader import load_prompt
 
 
 async def correct_sql(state: DataAgentState, runtime: Runtime[DataAgentContext]):
     runtime.stream_writer({"stage": "校正sql"})
     try:
-        return {}
+        # 获取相关数据
+        sql = state["sql"]
+        error = state["error"]
+        query = state["query"]
+        table_infos = state["table_infos"]
+        metric_infos = state["metric_infos"]
+        date_info = state["date_info"]
+        db_info = state["db_info"]
+
+        # 调用大模型矫正sql
+        prompt = PromptTemplate(template=load_prompt("correct_sql"),
+                                input_variables=["query", "table_infos", "metric_infos", "date_info", "db_info",
+                                                 "error", "sql"])
+        output_parser = StrOutputParser()
+        chain = prompt | llm | output_parser
+        sql = await  chain.ainvoke(
+            {
+                "query": query,
+                "table_infos": yaml.dump(table_infos, allow_unicode=True, sort_keys=False),
+                "metric_infos": yaml.dump(metric_infos, allow_unicode=True, sort_keys=False),
+                "date_info": yaml.dump(date_info, allow_unicode=True, sort_keys=False),
+                "db_info": yaml.dump(db_info, allow_unicode=True, sort_keys=False),
+                "sql": sql,
+                "error": error
+            }
+        )
+
+        logger.info(f"校正sql成功：{sql}")
+
+        return {"sql": sql}
+
+
     except Exception as e:
         logger.error(f"校验SQL失败：{str(e)}")
         raise
